daa2bfdffe5e5597aa68b1cdcfcf45b5cc9748d6
Arellys Villena <arellysvgamboa@gmail.com>
2026-05-23 10:46:46 -0500

    feat: add no discount strategy

---
+++ src/strategy/NoDiscountStrategy.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class NoDiscountStrategy implements DiscountStrategy {
+
+        @Override
+        public double applyDiscount(double total) {
+            return total;
+        }
+}
