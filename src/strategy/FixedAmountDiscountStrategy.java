7145f6f6c858781b063ccf5e97c8c0d64e599aae
Arellys Villena <arellysvgamboa@gmail.com>
2026-05-23 10:56:14 -0500

    feat: add fixed amount discount strategy

---
+++ src/strategy/FixedAmountDiscountStrategy.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public class FixedAmountDiscountStrategy implements DiscountStrategy {
+
+    private double amount;
+
+    public FixedAmountDiscountStrategy(double amount) {
+
+        this.amount = amount;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+
+        return total - amount;
+    }
+}
