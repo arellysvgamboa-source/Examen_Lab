82471028f36ca8327ba166b41103245773550216
Arellys Villena <arellysvgamboa@gmail.com>
2026-05-23 11:19:52 -0500

    feat: add credit card payment processor

---
+++ src/adapter/CreditCardPaymentProcessor.java
@@ -0,0 +1,9 @@
+package adapter;
+
+public class CreditCardPaymentProcessor implements PaymentProcessor {
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Pago con tarjeta: S/. " + amount);
+    }
+}
