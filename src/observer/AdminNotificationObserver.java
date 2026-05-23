2cee41748dc0acaf6597195059ac28134d214636
Arellys Villena <arellysvgamboa@gmail.com>
2026-05-23 11:28:03 -0500

    feat: add admin notification observer

---
+++ src/observer/AdminNotificationObserver.java
@@ -0,0 +1,10 @@
+package observer;
+
+public class AdminNotificationObserver implements OrderObserver {
+
+    @Override
+    public void update(String message) {
+
+        System.out.println("Notificando al administrador...");
+    }
+}
