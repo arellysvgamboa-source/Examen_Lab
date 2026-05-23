c57b2e9905d27cc7f2a18522aa13b5f719ffc8ea
Arellys Villena <arellysvgamboa@gmail.com>
2026-05-23 10:38:37 -0500

    feat: add product and cart models

---
+++ src/model/Cart.java
@@ -0,0 +1,24 @@
+package model;
+import java.util.*;
+
+public class Cart {
+
+    private List<Product> products;
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+}
