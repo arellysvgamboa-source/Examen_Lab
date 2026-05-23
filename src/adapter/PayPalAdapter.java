39ba8b1a3ad56abb2f26b94c74807504d58c641f
Arellys Villena <arellysvgamboa@gmail.com>
2026-05-23 11:14:31 -0500

    feat: add paypal adapter

---
+++ src/adapter/PayPalAdapter.java
@@ -0,0 +1,15 @@
+package adapter;
+
+public class PayPalAdapter implements PaymentProcessor {
+
+    private ExternalPayPalService payPalService;
+
+    public PayPalAdapter(ExternalPayPalService payPalService) {
+        this.payPalService = payPalService;
+    }
+
+    @Override
+    public void pay(double amount) {
+        payPalService.makePayment( "PEN", amount);
+    }
+}
